287fd61e2a3fc6de6aea711e5144f3f2aac39890
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-09-29 10:36:25 -0700

    Implementing Interfaces Using Subclasses

---
+++ InterfacesInJava/src/Main.java
@@ -0,0 +1,19 @@
+//implementation of interfaces in java
+
+interface Polygon {
+    void area(int length, int width);
+}
+
+class Rectangle implements Polygon {
+    public void area(int length, int width) {
+        System.out.println("Thea area of the Rectangle is:" +(length * width));
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle();
+
+        obj.area(50, 40);
+    }
+
+}
