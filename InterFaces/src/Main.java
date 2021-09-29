4840b398d289f81f1632f1b7e6dfbdcd8e18df4b
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-09-29 11:05:16 -0700

    Implementing Java Interface Example

---
+++ InterFaces/src/Main.java
@@ -0,0 +1,41 @@
+interface Polygon {
+    void area();
+
+    default void sides() {
+        System.out.println("We can get the number of sides of the Polygon");
+    }
+}
+
+class Rectangle implements Polygon {
+    public void area() {
+        int length = 40;
+        int width = 30;
+        int area = length * width;
+        System.out.println("The area of the rectangle is:" + area);
+    }
+
+    @Override
+    public void sides() {
+        System.out.println("A rectangle has Four sides");
+
+    }
+}
+
+class Square implements Polygon {
+    public void area() {
+        int length = 89;
+        int area = length * length;
+        System.out.println("The area of the Square:" +area);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle();
+        Square obj2 = new Square();
+        obj.area();
+        obj.sides();
+        obj2.area();
+
+        }
+    }
