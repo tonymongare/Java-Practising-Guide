9ca22955d70f7d6af17c80ea50be0344bae5deb2
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-09-29 11:52:48 -0700

    PolyMorphism in Java

---
+++ PolyMorphism/src/Main.java
@@ -0,0 +1,35 @@
+//Polymorphism
+class Music {
+    public void genre() {
+        System.out.println("Listening to Music...");
+    }
+}
+
+class Pop extends Music {
+    public void genre() {
+        System.out.println("Listening to PoP Music...");
+    }
+}
+class Rnb extends Music {
+    public void genre() {
+        System.out.println("Listening to Rnb Music...");
+    }
+}
+class Reggea extends Music {
+    public void genre() {
+        System.out.println("Listening to Reggea Music..");
+    }
+
+}
+class Main {
+
+    public static void main(String[] args) {
+        Pop popobj = new Pop();
+        Rnb Rbobj = new Rnb();
+        Reggea Reggobj = new Reggea();
+
+        popobj.genre();
+        Rbobj.genre();
+        Reggobj.genre();
+    }
+}
