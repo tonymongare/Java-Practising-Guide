c3054e4276cf43c1c593574bf3af1c094b9fc9a7
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-10-18 08:26:20 -0700

    Basic Example of Implemetation of Predefined Annotations

---
+++ Annotations in Java/src/Main.java
@@ -0,0 +1,34 @@
+//annotations in java
+
+class Animal {
+    public void display() {
+        System.out.println("All animals deserve a warm shelter");
+    }
+}
+
+//creating a subclass
+class Dog extends Animal {
+    @Override
+    public void display() {
+        System.out.println("Dogs are mans best friends");
+    }
+}
+//driver class
+class Main {
+    public static void main(String[] args) {
+        //creating object
+        Dog obj = new Dog();
+
+        //object returns subclass method instead of the superclass method
+
+        obj.display();
+    }
+}
+
+
+
+
+
+
+
+
