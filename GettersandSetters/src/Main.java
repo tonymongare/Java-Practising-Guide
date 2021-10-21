f3dbaf0a9458da9063359a5ee83636c03005a3dc
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-10-20 17:18:54 -0700

    Basic example of Using Getters and Setters in Java

---
+++ GettersandSetters/src/Main.java
@@ -0,0 +1,21 @@
+class Vehicle {
+    String color = "red";
+
+    //creating the setter method
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String c) {
+        this.color = c;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Vehicle obj = new Vehicle();
+
+        obj.setColor("Yellow");
+        System.out.println(getColor());
+    }
+}
