2f542fe5d54ad0366848a98df1abd7a6fb33572d
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-10-24 17:46:39 -0700

    Program to find a prime number in java

---
+++ Prime/src/Main.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+
+        int number = 11;
+        boolean label = false;
+
+        for(int i = 2; i <= number/2; i++) {
+            //checking for prime number
+            if(number % 2 == 0) {
+
+                label = true;
+                break;
+            }
+        }
+        if(!label) {
+            System.out.println("This is a prime Number");
+        } else {
+            System.out.println("This is not a prime number");
+        }
+    }
+}
