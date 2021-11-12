9d6bf193e8d1d0f2963cab4df809b3d9b4cab793
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-11-12 07:33:13 -0800

    A simple recursive function to find factorial of a number

---
+++ recursion.java
@@ -0,0 +1,19 @@
+class Factorial {
+
+    public static int factorial(int n) {
+
+        if (n != 0) {
+            return n * factorial(n - 1);
+        } else {
+            return 1;
+        }
+
+        }
+
+        public static void main(String[] args) {
+            int number = 6;
+            int result;
+            result = factorial(number);
+            System.out.println("The factorial of the number is: " + result);
+        }
+    }
