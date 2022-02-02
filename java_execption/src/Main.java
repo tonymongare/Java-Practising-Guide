d667d3e259faade61a8c14dedd38249ad8ff310a
isaac <tonyloimongare10@students.ku.ac.ke>
2022-02-01 20:37:19 -0800

    Exceptions in a Java

---
+++ java_execption/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(5/0);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Always executed after the try and catch block ");
+            //code that might have been skippes by return or break  or continue
+
+        }
+    }
+}
