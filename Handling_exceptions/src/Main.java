67b489903b6ababcd914b55df34d070e174bd285
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-02-28 16:54:41 +0300

    Add files via upload
    Handling exceptions in Java

---
+++ Handling_exceptions/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void divideByZero() {
+
+        throw new ArithmeticException("This is division by zero");
+    }
+    
+    public static void main(String[] args) {
+        divideByZero();
+
+        
+    }
+}
