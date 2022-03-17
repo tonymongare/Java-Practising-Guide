0d7f16caa4f70b02b966122ef859e7a73a70ede3
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-17 06:59:36 -0700

    Create Byte_Array_Input_Stream.java

---
+++ Byte_Array_Input_Stream.java
@@ -0,0 +1,25 @@
+import java.io.ByteArrayInputStream;
+
+class Main {
+    public static void main(String[] args) {
+        
+        byte[] array = {12,13,14,15};
+        
+        try {
+            
+            ByteArrayInputStream input = new ByteArrayInputStream(array);
+            
+            for (int i = 0; i < array.length; i++) {
+                
+                int data = input.read();
+                System.out.println(data + ", ");
+            }
+            
+            input.close();
+        }
+        
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
