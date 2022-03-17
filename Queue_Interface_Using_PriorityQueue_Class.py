48283195b4c1a16f6371145aa89d4c53c0200e38
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-17 02:33:00 -0700

    Create Queue_Interface_Using_PriorityQueue_Class.py

---
+++ Queue_Interface_Using_PriorityQueue_Class.py
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+class Main {
+    public static void main(String[] args) {
+        
+        Queue<Integer> numbers = new PriorityQueue<>();
+        
+        numbers.offer(22);
+        numbers.offer(23);
+        numbers.offer(24);
+        numbers.offer(25);
+        
+        System.out.println(numbers);
+        
+        int first_number = numbers.peek();
+        
+        System.out.println(first_number);
+        
+        int remove_head = numbers.poll();
+        
+        System.out.println(remove_head);
+        
+        
+    }
+}
