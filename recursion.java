ff1b72fcf5e99f73a51124cc287a9fa98440ef43
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-04-14 05:43:16 -0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,21 +1,16 @@
-//recursion
 class Factorial {
-
-    public static int factorial(int n) {
-
-        if (n != 0) {
-            return n * factorial(n - 1);
-        } else {
+    
+    static int factorial(int number) {
+        if (number != 0) {
+            return number * factorial(number- 1);
+        }
+        else {
             return 1;
         }
-
-        }
-
-        public static void main(String[] args) {
-            int number = 6;
-            int result;
-            result = factorial(number);
-            System.out.println("The factorial of the number is: " + result);
-        }
+    } 
+    public static void main(String[] args) {
+        int result;
+        result = factorial(10);
+        System.out.println(result);
     }
-
+}
